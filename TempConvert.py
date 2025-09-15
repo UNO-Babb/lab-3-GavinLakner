07f0e3fce114f1506905601b09602de33f45c2b3
GavinLakner <gavinlakner@icloud.com>
2025-09-15 03:52:52 +0000

    Num won't =

--- TempConvert.py
+++ TempConvert.py
@@ -12,13 +12,9 @@ num = num * 100
 num = int(num) 
 num = num / 100.0 
 print(num)
-
   #Prompt the user for a Fahrenheit temperature
-
   #Convert that temperature to celsius, rounding to 1 decimal percision
- 
   #Output converted temperature.
-
 print(tempF, "is ", tempC, "degrees celsius.")
 if __name__ == '__main__':
   main()
