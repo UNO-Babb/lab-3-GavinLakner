af8afb138616282f2cf911d426e16d57cf68d4d1
GavinLakner <gavinlakner@icloud.com>
2025-09-15 03:43:18 +0000

    More Errors but google says I'm doing it right

--- RPS.py
+++ RPS.py
@@ -1,13 +1,60 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name: Gavin Lakner
+#Date: 9/14/2025
+#Assignment: RPS
 import random
 
 def main():
   wins = 0
   ties = 0
   losses = 0
+
+game = input("Want to play Rock Paper Scissors? (Yes/No):")
+if game == "Yes": 
+  elif print("Goodbye!") == "No":
+  print("R = Rock, P = Paper, S = Scissors")
+  print('')
+  user = int(input("Choose Wisely:"))
+  print('')
+  'R' == 1
+  'P' == 2
+  'S' == 3
+  if user == 1 and computer == 1:
+    print("Must play again! We both played Rock!")
+  elif user == 1 and computer == 2:
+    print("You lose! You played Rock and I played Paper!")
+  elif user == 1 and computer == 3:
+    print("You win! You played Rock and I played Scissors!")
+  elif user == 2 and computer == 1:
+    print("You win! You played Paper and I played Rock!")
+  elif user == 2 and computer == 2:
+    print("Must play again! We both played Paper!")
+  elif user == 2 and computer == 3:
+    print("You lose! You played Paper and I played Scissors!")
+  elif user == 2 and computer == 1:
+    print("You lose! You played Scissors and I played Rock!")
+  elif user == 3 and computer == 2:
+    print("You win! You played Scissors and I played Paper!")
+  elif user == 3 and computer == 3:
+    print("Must play again! We both played Scissors!")
+  else:
+    print("Not a choice.")    
+
+  outcome = random.choice(["win", "lose", "tie"])
+  if outcome == "win":
+        print("You won this round!")
+        wins += 1
+  elif outcome == "lose":
+        print("You lost this round!")
+        losses += 1
+  else :outcome == "tie" 
+  print("This round was a tie!")
+  ties += 1
+
+  user_input = input("Would you like to play again? (yes/no): ").lower()
+  if user_input != 'yes':
+        play_again = False       
+
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
 
@@ -17,9 +64,9 @@ def main():
   #Ask the user if they would like to play again.
 
   #In the end, print the stats
-  print("Wins \t Ties \t Losses")
-  print("---- \t ---- \t ------")
-  print(wins, "\t", ties , "\t", losses)
+print("Wins \t Ties \t Losses")
+print("---- \t ---- \t ------")
+print(wins, "\t", ties , "\t", losses)
 
 if __name__ == '__main__':
   main()
--- TempConvert.py
+++ TempConvert.py
@@ -1,17 +1,24 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name: Gavin Lakner
+#Date: 9/11/2025
+#Assignment: TempConvert
 
 
 def main():
-  #Prompt the user for a Fahrenheit temperature
-  #Convert that temperature to celsius, rounding to 1 decimal percision
-  #Output converted temperature.
-  tempF = 80
 
-  tempC = tempF / 2
+  #Prompt the user for a Fahrenheit temperature
+
+  #Convert that temperature to celsius, rounding to 1 decimal percision
+ 
+  #Output converted temperature.
 
   print(tempF, "is ", tempC, "degrees celsius.")
 if __name__ == '__main__':
   main()
+num = float(print("Enter the temperature in Fahrenheit: "))
+(num - 32) * 5/9
+num = num * 100 
+num = int(num) 
+num = num / 100.0 
+print(num)
+
